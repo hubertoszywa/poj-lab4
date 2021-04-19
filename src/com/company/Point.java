75abfd15d60cf542f604bce153acce3ffec56846
Hubert Oszywa <46084687+hubertoszywa@users.noreply.github.com>
2021-04-19 10:35:26 +0200

    Class rebuild

--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -3,18 +3,17 @@ package com.company;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Point{
+public class Point {
 
-    double x;
-    double y;
-    double ab;
-    double bc;
-    double ca;
+    protected double x;
+    protected double y;
     Scanner sc = new Scanner(System.in);
 
-
+    // --- konstruktor - tworzenie punktu ---
     Point() {}
 
+
+    // --- settery ---
     public void setX(){
         while (true) {
             try {
@@ -39,46 +38,4 @@ public class Point{
         }
     }
 
-
-    public double countCircuit(){
-        return ab + bc + ca;
-    }
-
-
-
-    public double countArea(Point p1, Point p2, Point p3){
-        double temp = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
-        return Math.abs(temp) / 2;
-    }
-
-
-
-    public String kindOfTriangle() {
-
-        if (ab == bc && ab == ca && bc == ca)
-            return "Równoboczny";
-        else {
-            double sqa = Math.pow(ab, 2);
-            double sqb = Math.pow(bc, 2);
-            double sqc = Math.pow(ca, 2);
-
-            if (sqa == sqb + sqc || sqb == sqc + sqa || sqc == sqa + sqb) {
-                return "Prostokątny";
-            } else if (sqa > sqc + sqb || sqb > sqa + sqc || sqc > sqa + sqb) {
-                return "Rozwartokątny";
-            } else {
-                return "Ostrokątny";
-            }
-        }
-
-    }
-
-
-    public boolean checkTriangle(Point p1, Point p2, Point p3) {
-        ab = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-        bc = Math.sqrt(Math.pow(p3.x - p2.x, 2) + Math.pow(p3.y - p2.y, 2));
-        ca = Math.sqrt(Math.pow(p3.x - p1.x, 2) + Math.pow(p3.y - p1.y, 2));
-
-        return !(ab >= (bc + ca)) && !(ca >= (bc + ab)) && !(bc >= (ab + ca));
-    }
 }
--- src/com/company/Start.java
+++ src/com/company/Start.java
@@ -7,15 +7,13 @@ import java.util.Scanner;
 public class Start {
 
     int userChoice;
-    static boolean isTriangle = true;
-
-
 
     public static void pointsCoordinates() {
 
         System.out.println("-----------------------------------------------------");
         System.out.println("Podaj współrzędne kolejnych wierzchołków: ");
         System.out.println("-----------------------------------------------------");
+
         Point p1 = new Point();
         Point p2 = new Point();
         Point p3 = new Point();
@@ -27,18 +25,17 @@ public class Start {
         System.out.print("P3: x = "); p3.setX();
         System.out.print("    y = "); p3.setY();
 
-        isTriangle = p1.checkTriangle(p1, p2, p3);
+        Triangle t1 = new Triangle(p1, p2, p3);
 
-        if(isTriangle){
-            System.out.println("Obwód trójkąta jest równy: " + p1.countCircuit());
-            System.out.println("Pole trójkąta jest równe: " + p1.countArea(p1, p2, p3));
-            System.out.println("Rodzaj trójkąta: " + p1.kindOfTriangle());
+        if(t1.checkTriangle()){
+            System.out.println("Obwód trójkąta jest równy: " + t1.countPerimeter());
+            System.out.println("Pole trójkąta jest równe: " + t1.countArea());
+            System.out.println("Rodzaj trójkąta: " + t1.kindOfTriangle());
         } else {
             System.out.println("Z podanych wierzchołków nie uda się zbudować trójkąta!");
         }
+
     }
-
-
 
 
     public static void sideLengths() {
@@ -46,24 +43,21 @@ public class Start {
         System.out.println("-----------------------------------------------------");
         System.out.println("Podaj długości odcinków trójkąta: ");
         System.out.println("-----------------------------------------------------");
-        Length l1 = new Length();
 
-        System.out.print("|AB|: = "); l1.setAb();
-        System.out.print("|BC|: = "); l1.setBc();
-        System.out.print("|CA|: = "); l1.setCa();
+        Triangle t2 = new Triangle();
 
-        isTriangle = l1.checkTriangle();
+        System.out.print("|AB|: = "); t2.setAb();
+        System.out.print("|BC|: = "); t2.setBc();
+        System.out.print("|CA|: = "); t2.setCa();
 
-        if(isTriangle){
-            System.out.println("Obwód trójkąta jest równy: " + l1.countCircuit());
-            System.out.println("Pole trójkąta jest równe: " + l1.countArea());
-            System.out.println("Rodzaj trójkąta: " + l1.kindOfTriangle());
+
+        if(t2.checkTriangle()){
+            System.out.println("Obwód trójkąta jest równy: " + t2.countPerimeter());
+            System.out.println("Pole trójkąta jest równe: " + t2.countArea());
+            System.out.println("Rodzaj trójkąta: " + t2.kindOfTriangle());
         } else {
             System.out.println("Z podanych długości boków nie uda się zbudować trójkąta!");
         }
-
-
-
     }
 
 
@@ -97,14 +91,9 @@ public class Start {
             switch (userChoice) {
                 case 1:
                     pointsCoordinates();
-
-
-
                     break;
                 case 2:
                     sideLengths();
-
-
                     break;
                 case 3:
                     System.exit(0);
@@ -121,6 +110,7 @@ public class Start {
                         }
                     }
             } //end switch
+
         } //end while
 
     } //end constructor
